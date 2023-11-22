fd3f0d90992c91eb6e86ae16d85da7fd28c5a77b
SARAN1231 <saranprakash3112@gmail.com>
2023-11-22 22:18:48 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const http = require("http"); // Add this line to import the http module
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./Middleware/errorMiddleware");
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+connectDB();
+const app = express();
+const server = http.createServer(app); // Create an HTTP server
+
+app.use(express.json());
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+    // credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+});
